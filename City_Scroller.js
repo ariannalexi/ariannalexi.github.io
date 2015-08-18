52665e74d53e02668794f04deb7f8ed4d5d0f300
ariannalexi <arianna.solanki2017@innovationhighschool.org>
2015-08-18 15:43:25 -0400

    final(1)

--- City_Scroller.js
+++ City_Scroller.js
@@ -28,62 +28,82 @@ class Building {
 	}
 }
 
-//var frontScroller = [];
-//var middleScroller = [];
+var frontScroller = [];
+var middleScroller = [];
 var backScroller = [];
-//var frontSpeed = 0.2;
-//var middleSpeed = 0.4;
-var backSpeed =0.6;
+var frontSpeed = 0.2;
+var middleSpeed = 0.4;
+var backSpeed =0.8;
 
 void setup()
 {
-	size(800,600);
+	size(1875,1000);
 	background(17,9,89);
-
-	//for(i=0;i<frontScroller.length;i++){// Write a loop that adds buildings to the frontScroller. 
-		//height = random(400, 500);// Make the height a random number between 400 and 500 
-		//width = random(20,50);// and the width a random number between 20 and 50.
-		// HINT: Think about how to calculate the X and the Y.
-		//}
-
-	//for(i=0;i<middleScroller.length;i++){// Write a loop that adds buildings to the middleScroller. 
-		//height = random(300,400);// Make the height a random number between 300 and 400 
-		//width = random(100,250);// and the width a random number between 100 and 250.
-		// HINT: Think about how to calculate the X and the Y. 
-		//}
-
-	for(var i=0;i<100;i++){// Write a loop that adds buildings to the backScroller. 
+	var v=0;
+	for(i=0;i<frontScroller.length;i++){
+		var building3;
+		var bHeight= Math.floor((Math.random() *100 ) +200);
+		var bWidth = Math.floor((Math.random() *50 ) +20 );
+		var color=[210,100,10];
+		var x = v;
+		var y= 200 - bHeight; 
+		v = v + bWidth;
+		building3 = new Building(bWidth,bHeight, color, x, y);
+		frontScroller.push(building3);
+		}
+	var a=0;
+	for(i=0;i<middleScroller.length;i++){ 
+		var building2;
+		var bHeight= Math.floor((Math.random() *400 ) + 300);
+		var bWidth = Math.floor((Math.random() *250 ) +100 );
+		var color=[3,100,230];
+		var x = a;
+		var y= 600 - bHeight; 
+		a = a + bWidth;
+		building2 = new Building(bWidth,bHeight, color, x, y);
+		middleScroller.push(building2);
+		}
+		
+	var r = 0;
+	for(var i=0;i<100;i++){
 		var building1;
-		bHeight= random(100,200);// Make the height a random number between 100 and 200 
-		bWidth = random(300,400);// and the width a random number between 300 and 400.
-		color(185,185,185);
-		x = random(100,500);
-		y= random(100,600); //HINT: Think about how to calculate the X and the Y
-		building1 = new Building(bWidth,bHeight, color, x, y)
+		var bHeight= Math.floor((Math.random() * 400) + 500);
+		var bWidth = Math.floor((Math.random() * 400) + 300);
+		var color=[185,185,185];
+		var x = r;
+		var y= 1000 - bHeight; 
+		r = r + bWidth;
+		building1 = new Building(bWidth,bHeight, color, x, y);
 		backScroller.push(building1);
+		
 	}
+	
 }	
 
 
 void draw(){  
 	background(17,9,89); 
 
-	for(var i=0;i<backScroller.length;i++) { // Write a loop that loops through the back building 
+	for(var i=0;i<backScroller.length;i++) { 
 		backScroller[i].drawBuilding(); 
 	}
 
-		for(var i=0;i<backScroller.length;i++) 
-		{ // Write a loop that loops through the back building 
-		backScroller[i].move(0.6);// that moves and draws them. 
+	for(var i=0;i<backScroller.length;i++) { 
+		backScroller[i].move(0.6);
 	}
-	//for(var i=0;i<middleScroller.length;i++) {// Write a loop that loops through the middle building
-		//drawBuilding();
-		//move(0.4);// that moves and draws them. 
-		//}
+	for(var i=0;i<middleScroller.length;i++) {
+		middleScroller[i].drawBuilding();
+	}
+	
+	for(var i=0;i<middleScroller.length;i++){
+		middleScroller[i].move(0.4); 
+		}
 
-	//for(var i=0;i<frontScroller.length;i++) {// Write a loop that loops through the front building 
-		//drawBuilding();
-		//move(0.2);//that moves and draws them. 
-		//}
+	for(var i=0;i<frontScroller.length;i++) {
+		frontScroller[i].drawBuilding();
+		}
+	for(var i=0;i<frontScroller.length;i++) {
+		frontScroller[i].move(0.2); 
+		}
 
 }
